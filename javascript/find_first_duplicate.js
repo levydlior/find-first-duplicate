6ec42a35b2504954337738228a15b0f63d702c85
Lior <levydlior@gmail.com>
2022-06-03 11:09:27 -0600

    commit tree f3e1c1152c985ecc7d477241aae446a02198829c

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,19 +1,46 @@
 function findFirstDuplicate(arr) {
-  // type your code here
-}
+  const numbers = {}
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    console.log("numbers", numbers);
+    if (arr[i] in numbers) {
+      return arr[i];
+    }
+    numbers[arr[i]] = arr[i]
+  }
+  return -1;
+  }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+// add your own tests in here
 
-  console.log("");
+console.log("expecting: 5");
+console.log("=>", findFirstDuplicate([1, 5, 8, 3, 5]));
 
-  console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+console.log("Expecting: 3");
+console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+
+console.log("");
+
+console.log("Expecting: -1");
+console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+/*
+Given an array of number, I need to find if there are duplicates numbers, if there is a duplicate I need to return it.
+
+I can iterate once, make a set and if the number is in the set return it
+
+make new set
+iterate on arr
+each elemnt in arr becomes a key in new set
+if element in new set return element else return -1
+
+
+
+*/
 // And a written explanation of your solution
